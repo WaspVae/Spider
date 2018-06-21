99ef1abc7d4ad2cf5a37a13e3802f157be9832da
WaspVae <waspvae@gmail.com>
2018-06-21 15:41:34 +0800

    spider
    demo

---
+++ spider_wangyiyun_music.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import csv
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome(chrome_options=chrome_options)
+csv_file = open('playlist.csv', 'w', newline='', encoding='utf-8')
+writer = csv.writer(csv_file)
+writer.writerow(['标题', '播放数', '链接'])
+url = 'http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0'
+while url != 'javascript:void(0)':
+    browser.get(url)
+    browser.switch_to.frame('contentFrame')
+    data = browser.find_element_by_id('m-pl-container').find_elements_by_tag_name('li')
+    for i in range(len(data)):
+        nb = data[i].find_element_by_class_name('nb').text
+        if '万' in nb and int(nb.split('万')[0]) > 1000:
+            msk = data[i].find_element_by_css_selector('a.msk')
+            writer.writerow([msk.get_attribute('title'), nb, msk.get_attribute('href')])
+    url = browser.find_element_by_css_selector('a.zbtn.znxt').get_attribute('href')
+csv_file.close()
